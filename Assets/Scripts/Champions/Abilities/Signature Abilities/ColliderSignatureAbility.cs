d77fdd5415cafdd9c71ba3f707453007982435f8
Mannvika <45762765+Mannvika@users.noreply.github.com>
2025-12-15 16:59:30 -0500

    feat(Combat) started working on collider ability

--- Assets/Scripts/Champions/Abilities/Signature Abilities/ColliderSignatureAbility.cs
+++ Assets/Scripts/Champions/Abilities/Signature Abilities/ColliderSignatureAbility.cs
@@ -2,38 +2,24 @@ using UnityEngine;
 using Unity.Netcode;
 using System.Collections;
 
-[CreateAssetMenu(fileName = "SignatureAbility", menuName = "Scriptable Objects/Abilities/SignatureAbility/ProjectileSignature")]
+[CreateAssetMenu(fileName = "SignatureAbility", menuName = "Scriptable Objects/Abilities/SignatureAbility/ColliderSignature")]
 public class ColliderSignatureAbility : SignatureAbility
 {
-    public GameObject projectilePrefab;
-    public float projectileSpeed;
-    public float damage;
-    public int numberOfProjectiles;
-    public float timeBetweenProjectiles;
+    public GameObject colliderPrefab;
 
     public override IEnumerator SignatureRoutine(PlayerController parent, bool isServer)
     {
         parent.currentState = PlayerState.UsingSignatureAbility;
-        for(int i = 0; i < numberOfProjectiles; i++)
-        {
-            if (isServer)
-            {
-                SpawnProjectile(parent);
-            }
-            yield return new WaitForSeconds(timeBetweenProjectiles);
-        }
+        SpawnCollider(parent);
         OnEnd(parent, isServer);
+        yield return null;
     }
 
-    private void SpawnProjectile(PlayerController parent)
+    private void SpawnCollider(PlayerController parent)
     {
         Vector2 spawnPosition = (Vector2)parent.transform.position + ((Vector2)parent.transform.up * 1f);
-        GameObject projectile = Instantiate(projectilePrefab, spawnPosition, parent.transform.rotation);
-        NetworkProjectile proj = projectile.GetComponent<NetworkProjectile>();
-        proj.ShooterId = parent.OwnerClientId;
-        proj.speed.Value = projectileSpeed;
-        proj.damage = damage;
-        projectile.GetComponent<NetworkObject>().Spawn();
+        GameObject collider = Instantiate(colliderPrefab, spawnPosition, parent.transform.rotation);
+        collider.GetComponent<NetworkObject>().Spawn();
     }
     
 }
